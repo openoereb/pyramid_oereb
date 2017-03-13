d7659f9c28d823d0e874b30433d942a34c1d7be3
Francois Voisard <francois.voisard@ne.ch>
2017-03-13 16:08:24 +0100

    fixed typos again

--- pyramid_oereb/models.py
+++ pyramid_oereb/models.py
@@ -11,34 +11,34 @@ srid =2056
 class Example(Base):
     __tablename__ = "example"
     __table_args__ = {"schema": 'public'}
-    id = Column(sa.Integer, primary_key=True, autoincrement=True)
-    value = Column(sa.Text, nullable=False)
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    value = sa.Column(sa.Text, nullable=False)
 
 class Authority(Base):
     __tablename__ = 'authority'
     __table_args__ = {'schema': schema}
-    id = Column(sa.String, primary_key=True)
-    name = Column(sa.String)
-    url = Column(sa.String)
+    id = sa.Column(sa.String, primary_key=True)
+    name = sa.Column(sa.String)
+    url = sa.Column(sa.String)
 
 class Topic(Base):
     __tablename__ = 'topic'
     __table_args__ = {'schema': schema}
-    id = Column(sa.Integer, primary_key=True)
-    name = Column(sa.String)
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String)
 
 #railway_construction_limits
 class RailwayConstructionLimits(Base):
     __tablename__ = 'plr_97'
     __plr_id__ = 97
     __table_args__ = {'schema': schema}
-    id = Column(sa.Integer, primary_key=True)
-    tenor = Column(sa.String)
-    topic_id = Column(sa.Integer, ForeignKey(Topic.id), nullable=False)
-    # subtopic = Column(sa.String)
-    # other_topic = Column(sa.String)
-    code = Column(sa.String)
-    legal_state  = Column(sa.String)
-    publication_date = Column(sa.Date)
-    authority_id = Column(sa.Integer, ForeignKey(Authority.id), nullable=False)
-    geom = Column(Geometry('MULTILINESTRING', dimension=2, srid=srid))
+    id = sa.Column(sa.Integer, primary_key=True)
+    tenor = sa.Column(sa.String)
+    topic_id = sa.Column(sa.Integer, ForeignKey(Topic.id), nullable=False)
+    # subtopic = sa.Column(sa.String)
+    # other_topic = sa.Column(sa.String)
+    code = sa.Column(sa.String)
+    legal_state  = sa.Column(sa.String)
+    publication_date = sa.Column(sa.Date)
+    authority_id = sa.Column(sa.Integer, ForeignKey(Authority.id), nullable=False)
+    geom = sa.Column(Geometry('MULTILINESTRING', dimension=2, srid=srid))
