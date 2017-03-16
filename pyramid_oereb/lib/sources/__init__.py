edcf2ffac178e18fd91a37febe1e38bd7e95b826
Karsten Deininger <karsten.deininger@bl.ch>
2017-03-16 15:54:57 +0100

    Add documentation

--- pyramid_oereb/lib/sources/__init__.py
+++ pyramid_oereb/lib/sources/__init__.py
@@ -5,7 +5,13 @@ class Base(object):
     records = list()
 
     def __init__(self):
+        """
+        Base class for all sources.
+        """
         pass
 
     def read(self):
+        """
+        Every source class has to implement a read method.
+        """
         pass
