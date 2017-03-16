4c32064f66414677ada3a11bc90dc7283dfd9b2f
Clemens Rudert <clemens.rudert@bl.ch>
2017-03-16 17:16:08 +0100

    GSOREB-35 #time 10m add doc string

--- pyramid_oereb/lib/sources/plr.py
+++ pyramid_oereb/lib/sources/plr.py
@@ -12,12 +12,24 @@ class PlrBaseSource(Base):
 class PlrDatabaseSource(PlrBaseSource):
 
     def __init__(self, adapter, model):
+        """
+        The plug for plrs which uses a database as source.
+        :param adapter: The database adapter which provides access to the desired database session
+        :type adapter: pyramid_oereb.lib.adapter.DatabaseAdapter
+        :param model: The orm to map database source to plr style
+        :type model: sqlalchemy.ext.declarative.DeclarativeMeta
+        """
         super(PlrDatabaseSource, self).__init__()
         self._adapter_ = adapter
         self._model_ = model
 
-    def read(self):
-        session = self._adapter_.get_session()
+    def read(self, key):
+        """
+        Central method to read all plrs.
+        :param key: Key of the desired database connection
+        :type key: str
+        """
+        session = self._adapter_.get_session(key)
         results = session.query(self._model_).all()
         self.records = list()
         for r in results:
