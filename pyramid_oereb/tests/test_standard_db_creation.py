731096a65849bcea36524f51f0d5108cec54c58c
Clemens Rudert <clemens.rudert@bl.ch>
2017-03-17 11:55:11 +0100

    use a test specific config file

--- pyramid_oereb/tests/test_standard_db_creation.py
+++ pyramid_oereb/tests/test_standard_db_creation.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
 import pytest
 
 from pyramid.config import ConfigurationError
@@ -11,8 +10,4 @@ __create_date__ = '16.03.17'
 @pytest.mark.run(order=1)
 def test_create_standard_db():
     from pyramid_oereb.standard import create_tables
-    db_url = os.environ.get('SQLALCHEMY_URL')
-    if db_url:
-        create_tables(connection_string=db_url)
-    else:
-        raise ConfigurationError()
+    create_tables(configuration_yaml_path='pyramid_oereb_test.yml')
