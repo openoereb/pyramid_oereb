66a03d0fdd51e7f72cf2174277ace5012062f8bf
Karsten Deininger <karsten.deininger@bl.ch>
2017-03-27 18:08:42 +0200

    Add get_capabilties method and test file
    GSOREB-18 #comment Add get_capabilties method and test file. Referenced schema definition still missing.

--- pyramid_oereb/tests/test_webservice_getcapabilities.py
+++ pyramid_oereb/tests/test_webservice_getcapabilities.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
+import json
+
+from jsonschema import validate
+from pyramid.testing import DummyRequest
+
+from pyramid_oereb.views.webservice import PlrWebservice
 
 
 def test_getcapabilities():
-    assert True
+    service = PlrWebservice(DummyRequest())
+    with open('./pyramid_oereb/tests/resources/schema_webservices.json') as f:
+        schema = json.load(f)
+    caps = service.get_capabilities()
+    validate(caps, schema)
