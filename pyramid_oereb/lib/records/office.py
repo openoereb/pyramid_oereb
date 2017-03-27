aa7ffc0a7104c117e92d27a00597bb559960efa1
Francois Voisard <francois.voisard@ne.ch>
2017-03-27 15:06:44 +0200

    reordered attribute priority and fixed lint errors

--- pyramid_oereb/lib/records/office.py
+++ pyramid_oereb/lib/records/office.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
-class AuthorityRecord(object):
-    uid = None
+class OfficeRecord(object):
     name = None
+    uid = None
     office_at_web = None
     line1 = None
     line2 = None
@@ -12,14 +12,14 @@ class AuthorityRecord(object):
     postal_code = None
     city = None
 
-    def __init__(self, uid = None, name = None, office_at_web = None, line1 = None, line2 = None, 
-        street = None, number = None, postal_code = None, city = None):
+    def __init__(self, name=None, uid=None, office_at_web=None, line1=None, line2=None,
+                 street=None, number=None, postal_code=None, city=None):
         """
-        Competent authority record.
-        :param uid:             The unique identifier of the authority in the federal register
-        :type  uid:             str
+        Responsible office record.
         :param name:               The official name of the authority
         :type  name:               str
+        :param uid:             The unique identifier of the authority in the federal register
+        :type  uid:             str
         :param office_at_web:               The URL to the office's homepage
         :type office_at_web:               str
         :param line1:               Complementary address information
@@ -30,26 +30,25 @@ class AuthorityRecord(object):
         :type street:               str
         :param number:               House number
         :type number:               str
-        :param postal_code:               ZIP Code of the 
+        :param postal_code:               ZIP Code of the
         :type postal_code:               integer
         :param city:               The city where the authority is located
         :type postal_code:               str
         """
 
-        if not (uid and name):
-            raise TypeError('Fields "uid" and "name" must be defined. '
-                            'Got {0}, {1}.'.format(uid, name))
+        if not (name):
+            raise TypeError('Field "name" must be defined. '
+                            'Got {0} .'.format(name))
 
-        self.uid = uid
         self.name = name
+        self.uid = uid
         self.office_at_web = office_at_web
         self.line1 = line1
         self.line2 = line2
         self.street = street
-        self.number = type_code
-        self.postal_code = type_code_list
-        self.city = view_service
-        self.basis = basis
+        self.number = number
+        self.postal_code = postal_code
+        self.city = city
 
     @classmethod
     def get_fields(cls):
@@ -58,6 +57,7 @@ class AuthorityRecord(object):
         :return:    List of available field names.
         :rtype:     list
         """
+
         return [
             'uid',
             'name',
