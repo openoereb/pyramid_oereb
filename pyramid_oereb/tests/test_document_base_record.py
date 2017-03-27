0dfe591e6bd8cb4b4184d50cb5d5e6d9ad148256
Clemens Rudert <clemens.rudert@bl.ch>
2017-03-27 16:22:14 +0200

    GSOREB-138 #time 15m implement test for document base class

---
+++ pyramid_oereb/tests/test_document_base_record.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import pytest
+import datetime
+from pyramid_oereb.lib.records.documents import DocumentBase
+
+
+def test_get_fields():
+    expected_fields = [
+        'text_at_web',
+        'law_status',
+        'published_from'
+    ]
+    fields = DocumentBase.get_fields()
+    assert fields == expected_fields
+
+
+def test_mandatory_fields():
+    with pytest.raises(TypeError):
+        DocumentBase()
+
+
+def test_init():
+    record = DocumentBase("runningModifications", datetime.date(1985, 8, 29))
+    assert record.law_status == "runningModifications"
+    assert record.text_at_web is None
+    assert isinstance(record.published_from, datetime.date)
