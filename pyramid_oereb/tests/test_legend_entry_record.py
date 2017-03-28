bc3e35bc5e62b9c09d9caf2061792dfa24d5714f
Clemens Rudert <clemens.rudert@bl.ch>
2017-03-28 11:06:06 +0200

    GSOREB-142 #time 30m implement legend entry record class test

---
+++ pyramid_oereb/tests/test_legend_entry_record.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import pytest
+from pyramid_oereb.lib.records.view_service import LegendEntryRecord
+
+
+__author__ = 'Clemens Rudert'
+__create_date__ = '28.03.17'
+
+
+def test_get_fields():
+    expected_fields = [
+        'symbol',
+        'legend_text',
+        'type_code',
+        'type_code_list',
+        'theme',
+        'sub_theme',
+        'additional_theme'
+    ]
+    fields = LegendEntryRecord.get_fields()
+    assert fields == expected_fields
+
+
+def test_mandatory_fields():
+    with pytest.raises(TypeError):
+        LegendEntryRecord()
+
+
+def test_init():
+    record = LegendEntryRecord(bin(100), 'test', 'test_code', 'test', 'test')
+    assert isinstance(record.symbol, bin)
+    assert isinstance(record.legend_text, str)
+    assert isinstance(record.type_code, str)
+    assert isinstance(record.type_code_list, str)
+    assert isinstance(record.theme, str)
+    assert record.sub_theme is None
+    assert record.additional_theme is None
