5b51c244862bbb92fcc7059d52bdee304b67b827
Clemens Rudert <clemens.rudert@bl.ch>
2017-03-28 11:23:31 +0200

    GSOREB-142 #time 5mfix view service record class test

--- pyramid_oereb/tests/test_legend_entry_record.py
+++ pyramid_oereb/tests/test_legend_entry_record.py
@@ -28,7 +28,7 @@ def test_mandatory_fields():
 
 def test_init():
     record = LegendEntryRecord(bin(100), 'test', 'test_code', 'test', 'test')
-    assert isinstance(record.symbol, bin)
+    assert isinstance(record.symbol, str)
     assert isinstance(record.legend_text, str)
     assert isinstance(record.type_code, str)
     assert isinstance(record.type_code_list, str)
