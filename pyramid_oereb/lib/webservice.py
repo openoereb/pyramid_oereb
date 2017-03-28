de7f59e46a761d28f7ea6380ecacdab97f558772
Karsten Deininger <karsten.deininger@bl.ch>
2017-03-28 12:06:14 +0200

    Add configuration reader for webservices
    GSOREB-18 #comment Add configuration reader for webservices.

---
+++ pyramid_oereb/lib/webservice.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from pyramid_oereb.lib.config import parse
+
+
+class ConfigReader(object):
+    def __init__(self, config_file, config_section):
+        """
+        Loads configuration from yaml file and provides methods for generating webservice output.
+        :param config_file: The configuration yaml file.
+        :type config_file: str
+        :param config_section: The section within the yaml file.
+        :type config_section: str
+        """
+        self.__config__ = parse(config_file, config_section)
+
+    def get_crs(self):
+        crs = list()
+        srid = self.__config__.get('srid')
+        if srid:
+            crs.append(u'epsg:' + unicode(srid))
+        return crs
