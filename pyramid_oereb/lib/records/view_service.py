5922ffcccbc7a1a4269f41ec0f6b28584bcd0d39
Clemens Rudert <clemens.rudert@bl.ch>
2017-03-28 10:37:13 +0200

    GSOREB-143 #time 30m implement view service record class

--- pyramid_oereb/lib/records/view_service.py
+++ pyramid_oereb/lib/records/view_service.py
@@ -4,12 +4,12 @@ __author__ = 'Clemens Rudert'
 __create_date__ = '27.03.17'
 
 
-class LegendEntry(object):
+class LegendEntryRecord(object):
 
     def __init__(self, symbol=None, legend_text=None, type_code=None, type_code_list=None, theme=None,
                  sub_theme=None, additional_theme=None):
         """
-
+        Represents a legend entry with it's text as well as it's image.
         :param symbol: The binary file content of the legend image.
         :type symbol: binary
         :param legend_text: The description text for the legend entry.
@@ -43,7 +43,7 @@ class LegendEntry(object):
         """
         Returns a list of available field names.
         :return: List of available field names.
-        :rtype: list
+        :rtype: list of str
         """
         return [
             'symbol',
@@ -53,4 +53,40 @@ class LegendEntry(object):
             'theme',
             'sub_theme',
             'additional_theme'
+        ]
+
+
+class ViewServiceRecord(object):
+
+    def __init__(self, link_wms=None, legend_web=None, legends=None):
+        """
+
+        :param link_wms: The link URL to the actual service (WMS)
+        :type link_wms: str
+        :param legend_web: The link URL to the actual legend service (WMS get legend)
+        :type legend_web: str
+        :param legends: A list of all relevant legend entries.
+        :type legends: list of LegendEntry
+        """
+        if not (link_wms and legend_web):
+            raise TypeError('Fields "link_wms" and "legend_web" must be defined. Got {0} and {1}.'.format(
+                link_wms, legend_web))
+        self.link_wms = link_wms
+        self.legend_web = legend_web
+        if legends is None:
+            self.legends = []
+        else:
+            self.legends = legends
+
+    @classmethod
+    def get_fields(cls):
+        """
+        Returns a list of available field names.
+        :return: List of available field names.
+        :rtype: list of str
+        """
+        return [
+            'link_wms',
+            'legend_web',
+            'legends'
         ]
