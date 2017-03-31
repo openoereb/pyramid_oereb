1358f6732647ed9be7518e42d216753239cdea73
Clemens Rudert <clemens.rudert@bl.ch>
2017-03-31 08:43:19 +0200

    GSOREB-22 #time 30m set configuration on server startup

--- pyramid_oereb/__init__.py
+++ pyramid_oereb/__init__.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import logging
+
+from pyramid_oereb.lib.adapter import DatabaseAdapter
+from pyramid_oereb.lib.config import ConfigReader
 from pyramid.config import Configurator
 
 from pyramid_oereb.lib.config import parse
+from pyramid_oereb.lib.readers.real_estate import RealEstateReader
+
 __version__ = '0.0.1'
 
 
 log = logging.getLogger('pyramid_oereb')
 route_prefix = None
+config_reader = None
+database_adapter = None
 
 
 def main(global_config, **settings):
@@ -31,7 +38,7 @@ def includeme(config):
     :param config: The pyramid apps config object
     :type config: Configurator
     """
-    global route_prefix
+    global route_prefix, config_reader, database_adapter
 
     # Set route prefix
     route_prefix = config.route_prefix
@@ -42,6 +49,18 @@ def includeme(config):
     # Load configuration file
     cfg_file = settings.get('pyramid_oereb.cfg.file', None)
     cfg_section = settings.get('pyramid_oereb.cfg.section', None)
+    config_reader = ConfigReader(cfg_file, cfg_section)
+    real_estate_config = config_reader.get_real_estate_config()
+
+    # initially instantiate database adapter for global session handling
+    database_adapter = DatabaseAdapter()
+
+
+    real_estate_reader = RealEstateReader(
+        real_estate_config.get('source').get('class'),
+        **real_estate_config.get('source').get('params')
+    )
+
     settings.update({
         'pyramid_oereb': parse(cfg_file, cfg_section)
     })
