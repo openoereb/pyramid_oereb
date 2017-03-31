8e55e6c48a29c960fc8a0c6c26800dc9210f1d9f
Clemens Rudert <clemens.rudert@bl.ch>
2017-03-31 13:41:46 +0200

    GSOREB-22 #time 10m create tests for real estate reader

---
+++ pyramid_oereb/tests/test_real_estate_reader.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import pytest
+from sqlalchemy.orm.exc import NoResultFound
+
+from pyramid_oereb.lib.sources import Base
+from pyramid_oereb.lib.readers.real_estate import RealEstateReader
+from pyramid_oereb.tests.conftest import config_reader
+
+
+@pytest.mark.run(order=2)
+def test_init():
+    reader = RealEstateReader(
+        config_reader.get_real_estate_config().get('source').get('class'),
+        **config_reader.get_real_estate_config().get('source').get('params')
+    )
+    assert isinstance(reader._source_, Base)
+
+
+@pytest.mark.run(order=2)
+@pytest.mark.parametrize("param", [
+    {'nb_ident': 'BL0200002789', 'number': '545'},
+    {'egrid': 'CH1234'}
+])
+def test_read(param):
+    reader = RealEstateReader(
+        config_reader.get_real_estate_config().get('source').get('class'),
+        **config_reader.get_real_estate_config().get('source').get('params')
+    )
+    with pytest.raises(NoResultFound):
+        reader.read(**param)
+
+# TODO: Implement tests for return values, not possible now, cause there is no data in database
