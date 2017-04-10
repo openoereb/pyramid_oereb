94918bc7f626ef041b65a8aae9f991104ee8a00c
Francois Voisard <francois.voisard@ne.ch>
2017-04-10 11:11:42 +0200

    added glossary record

---
+++ pyramid_oereb/lib/records/glossary.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+class GlossaryRecord(object):
+
+    def __init__(self, title, content):
+        """
+        Represents a glossary entry with the term and it's description.
+        :param title: The term used in the extract
+        :type title: strint
+        :param content: The description text for the glossary entry.
+        :type content: str
+        """
+        self.title = title
+        self.content = content
+
+    @classmethod
+    def get_fields(cls):
+        """
+        Returns a list of available field names.
+        :return: List of available field names.
+        :rtype: list of str
+        """
+        return [
+            'title',
+            'content'
+        ]
