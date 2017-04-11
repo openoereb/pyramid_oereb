8008c9e4f4138d541bcf208984a6f742685b58ff
Clemens Rudert <clemens.rudert@bl.ch>
2017-04-11 09:34:29 +0200

    GSOREB-126 #time 10m adapt address reader and source to be more straight with it's parameters

--- pyramid_oereb/lib/readers/address.py
+++ pyramid_oereb/lib/readers/address.py
@@ -9,21 +9,25 @@ class AddressReader(object):
         The central reader accessor for addresses inside the application.
         :param dotted_source_class_path: The path to the class which represents the source used by this
         reader. This class must exist and it must implement basic source behaviour.
-        :type dotted_source_class_path: str
+        :type dotted_source_class_path: str or pyramid_oereb.lib.sources.address.AddressBaseSource
         :param params: kwargs, which are necessary as configuration parameter for the above by dotted name
         defined class.
         :type: kwargs
         """
-        source_class = DottedNameResolver().resolve(dotted_source_class_path)
+        source_class = DottedNameResolver().maybe_resolve(dotted_source_class_path)
         self._source_ = source_class(**params)
 
-    def read(self, **kwargs):
+    def read(self, street_name, zip_code, street_number):
         """
         The central read accessor method to get all desired records from configured source.
-        :param kwargs: the kwargs which are necessary as parameters for querying the address source.
-        :type kwargs: dict
-        :return: the list of all found records
+        :param street_name: The name of the street for the desired address.
+        :type street_name: unicode
+        :param zip_code: The postal zipcode for the desired address.
+        :type zip_code: int
+        :param street_number: The house or so called street number of the desired address.
+        :type street_number: str
+        :returns: The list of found records.
         :rtype: list of pyramid_oereb.lib.records.address.AddressRecord
         """
-        self._source_.read(**kwargs)
+        self._source_.read(street_name, zip_code, street_number)
         return self._source_.records
