6677ca07fa95d58fe6d65b76a9dcf6504bdb94af
Karsten Deininger <karsten.deininger@bl.ch>
2017-04-13 08:35:22 +0200

    Add to_extract method to geometry record
    GSOREB-160 #comment Add to_extract method to geometry record including tests.
    GSOREB-160 #time 30m Add to_extract method to geometry record including tests.

--- pyramid_oereb/tests/test_geometry_record.py
+++ pyramid_oereb/tests/test_geometry_record.py
@@ -2,8 +2,10 @@
 
 import datetime
 import pytest
+from shapely.geometry import Point
 
 from pyramid_oereb.lib.records.geometry import GeometryRecord
+from pyramid_oereb.lib.records.office import OfficeRecord
 
 
 def test_get_fields():
@@ -25,10 +27,24 @@ def test_mandatory_fields():
 
 
 def test_init():
-    record = GeometryRecord("runningModifications", datetime.date(1985, 8, 29), 'test')
+    record = GeometryRecord('runningModifications', datetime.date(1985, 8, 29), 'test')
     assert isinstance(record.legal_state, str)
     assert isinstance(record.published_from, datetime.date)
     assert isinstance(record.geo_metadata, str)
     assert record.geom is None
     assert record.public_law_restriction is None
     assert record.office is None
+
+
+def test_to_extract():
+    office = OfficeRecord('Office')
+    record = GeometryRecord('runningModifications', datetime.date(1985, 8, 29), 'test',
+                            geom=Point((0, 0)), office=office)
+    assert record.to_extract() == {
+        'legal_state': 'runningModifications',
+        'geo_metadata': 'test',
+        'geom': Point((0, 0)),
+        'office': {
+            'name': 'Office'
+        }
+    }
