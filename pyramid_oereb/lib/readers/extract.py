c5e3a2635015da83fd672e96bddf66df5028e9a0
Clemens Rudert <clemens.rudert@bl.ch>
2017-04-26 17:32:14 +0200

    GSOREB-161 #time 1h reorganize initialisation of server, implement logo creation, integrate logos in extract, add tests

--- pyramid_oereb/lib/readers/extract.py
+++ pyramid_oereb/lib/readers/extract.py
@@ -4,24 +4,61 @@ from pyramid_oereb.lib.records.extract import ExtractRecord
 
 class ExtractReader(object):
 
-    def __init__(self, plr_sources):
+    def __init__(self, plr_sources, plr_cadastre_authority, logos):
 
         """
         The central reader accessor for the extract inside the application.
         :param dotted_source_class_path: The path to the class which represents the source used by this
         reader. This class must exist and it must implement basic source behaviour.
         :type dotted_source_class_path: str or pyramid_oereb.lib.sources.extract.ExtractBaseSource
+        :param plr_cadastre_authority: The authority responsible for the PLR cadastre.
+        :type plr_cadastre_authority: pyramid_oereb.lib.records.office.OffcieRecord
+        :param logos: The logos of confederation, canton and oereb wrapped in a LogoRecord
+        :type logos: dict
         """
         self.extract = None
         self._plr_sources_ = plr_sources
+        self._plr_cadastre_authority_ = plr_cadastre_authority
+        self._logos_ = logos
 
-    def read(self, real_estate, plr_cadastre_authority):
+    @property
+    def plr_cadastre_authority(self):
+        """
+        Returns the authority responsible for the PLR cadastre.
+        :return: The authority responsible for the PLR cadastre.
+        :rtype: pyramid_oereb.lib.records.office.OffcieRecord
+        """
+        return self._plr_cadastre_authority_
+
+    @property
+    def logo_plr_cadastre(self):
+        """
+        :return: The logo for oereb as a LogoRecord.
+        :rtype: pyramid_oereb.lib.records.logo.LogoRecord
+        """
+        return self._logos_.get('oereb')
+
+    @property
+    def federal_logo(self):
+        """
+        :return: The federal logo as a LogoRecord.
+        :rtype: pyramid_oereb.lib.records.logo.LogoRecord
+        """
+        return self._logos_.get('confederation')
+
+    @property
+    def cantonal_logo(self):
+        """
+        :return: The cantonal logos as a LogoRecord.
+        :rtype: pyramid_oereb.lib.records.logo.LogoRecord
+        """
+        return self._logos_.get('canton')
+
+    def read(self, real_estate):
         """
         The central read accessor method to get all desired records from configured source.
         :param real_estate: The real estate for which the report should be generated
         :type real_estate: pyramid_oereb.lib.records.real_estate.RealEstateRecord
-        :param plr_cadastre_authority: The authority responsible for the PLR cadastre.
-        :type plr_cadastre_authority: pyramid_oereb.lib.records.office.OffcieRecord
         :return: The extract record containing all gathered data.
         :rtype: pyramid_oereb.lib.records.extract.ExtractRecord
         """
@@ -29,10 +66,10 @@ class ExtractReader(object):
             plr_source.read(real_estate)
         self.extract = ExtractRecord(
             real_estate,
+            self.logo_plr_cadastre,
+            self.federal_logo,
+            self.cantonal_logo,
             bin(100),
-            bin(100),
-            bin(100),
-            bin(100),
-            plr_cadastre_authority
+            self.plr_cadastre_authority
         )
         return self.extract
--- pyramid_oereb/tests/conftest.py
+++ pyramid_oereb/tests/conftest.py
@@ -20,6 +20,8 @@ class MockRequest(DummyRequest):
         super(MockRequest, self).__init__()
         real_estate_config = config_reader.get_real_estate_config()
         municipality_config = config_reader.get_municipality_config()
+        logos = config_reader.get_logo_config()
+        plr_cadastre_authority = config_reader.get_plr_cadastre_authority()
 
         real_estate_reader = RealEstateReader(
             real_estate_config.get('source').get('class'),
@@ -38,14 +40,15 @@ class MockRequest(DummyRequest):
             plr_sources.append(PlrStandardDatabaseSource(**plr))
 
         extract_reader = ExtractReader(
-            plr_sources
+            plr_sources,
+            plr_cadastre_authority,
+            logos
         )
         self.processor = Processor(
             real_estate_reader,
             municipality_reader,
             plr_sources,
-            extract_reader,
-            config_reader.get_plr_cadastre_authority()
+            extract_reader
         )
 
     @property
