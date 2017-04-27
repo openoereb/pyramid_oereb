50042a0105c73fe3bc0f1cc32f70088ebc616b71
Clemens Rudert <clemens.rudert@bl.ch>
2017-04-27 13:45:48 +0200

    GSOREB-166 #time 30m organize models under folder now

---
+++ pyramid_oereb/models/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+
+NAMING_CONVENTION = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = sa.MetaData(naming_convention=NAMING_CONVENTION)
+Base = declarative_base()
