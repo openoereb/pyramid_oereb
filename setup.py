f0fea62f9d135027347e6a2585b67e346921d262
Clemens Rudert <clemens.rudert@bl.ch>
2017-05-05 15:27:57 +0200

    GSOREB-126 #time 30m fix version to first alpha release

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ requires = [
 
 setup(
     name='pyramid_oereb',
-    version='0.0.1',
+    version='1.0.0-alpha.1',
     description='pyramid_oereb, extension for pyramid web frame work to provide '
             'a basic server part for the oereb project',
     long_description=README + '\n\n' + CHANGES,
