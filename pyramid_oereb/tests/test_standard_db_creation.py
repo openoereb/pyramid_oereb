7abf78987d48577c9eabd16564fef42863e03896
Karsten Deininger <karsten.deininger@bl.ch>
2017-05-22 11:48:26 +0200

    Move test configuration yaml into tests folder
    GSOREB-178 #comment Move test configuration yaml into tests folder.

--- pyramid_oereb/tests/test_standard_db_creation.py
+++ pyramid_oereb/tests/test_standard_db_creation.py
@@ -5,12 +5,18 @@ import pytest
 @pytest.mark.run(order=1)
 def test_create_standard_db():
     from pyramid_oereb.standard import _create_tables_from_standard_configuration_
-    _create_tables_from_standard_configuration_(configuration_yaml_path='pyramid_oereb_test.yml')
+    _create_tables_from_standard_configuration_(
+        configuration_yaml_path='pyramid_oereb/tests/resources/pyramid_oereb_test.yml'
+    )
 
 
 @pytest.mark.run(order=0)
 def test_drop_tables():
     from pyramid_oereb.standard import _create_tables_from_standard_configuration_, \
         _drop_tables_from_standard_configuration_
-    _create_tables_from_standard_configuration_(configuration_yaml_path='pyramid_oereb_test.yml')
-    _drop_tables_from_standard_configuration_(configuration_yaml_path='pyramid_oereb_test.yml')
+    _create_tables_from_standard_configuration_(
+        configuration_yaml_path='pyramid_oereb/tests/resources/pyramid_oereb_test.yml'
+    )
+    _drop_tables_from_standard_configuration_(
+        configuration_yaml_path='pyramid_oereb/tests/resources/pyramid_oereb_test.yml'
+    )
