9f481e04e2eb05d4e78f60251e464c144230ef1c
Francois Voisard <francois.voisard@ne.ch>
2017-06-19 16:36:07 +0200

    added missing whiteline at end

--- pyramid_oereb/standard/get_cadastral_data_update.py
+++ pyramid_oereb/standard/get_cadastral_data_update.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
+
+def get_update_date_from_file():
+    """
+    Gets the date of the latest update of the used data from a file
+    """
+
+    return
+
+
+def get_update_date_from_db():
+    """
+    Gets the date of the latest update of the used data from a table
+    """
+
+    return
+
+
 def get_surveying_data_update_date():
     """
     Gets the date of the latest update of the used cadastral data for the
     situation map.
     """
 
-    update_date = datetime.now().strftime("%d.%m.%Y-%H:%M:%S")
+    update_date = datetime.date.today()
 
     return update_date
