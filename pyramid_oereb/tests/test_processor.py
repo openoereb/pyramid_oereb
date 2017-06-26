a22010edff170a4bf9fb07cac860542c14c4b491
Clemens Rudert <clemens.rudert@bl.ch>
2017-06-26 15:34:01 +0200

    GSOREB-189 #time 1h implement tests

--- pyramid_oereb/tests/test_processor.py
+++ pyramid_oereb/tests/test_processor.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# import datetime
+import datetime
 import pytest
 from pyramid_oereb.lib.processor import Processor
 from pyramid_oereb.lib.records.extract import ExtractRecord
-# from pyramid_oereb.lib.records.image import ImageRecord
-# from pyramid_oereb.lib.records.office import OfficeRecord
-# from pyramid_oereb.lib.records.plr import PlrRecord
-# from pyramid_oereb.lib.records.theme import ThemeRecord
-# from pyramid_oereb.lib.records.view_service import ViewServiceRecord, LegendEntryRecord
+from pyramid_oereb.lib.records.image import ImageRecord
+from pyramid_oereb.lib.records.office import OfficeRecord
+from pyramid_oereb.lib.records.plr import PlrRecord
+from pyramid_oereb.lib.records.theme import ThemeRecord
+from pyramid_oereb.lib.records.view_service import ViewServiceRecord, LegendEntryRecord
 from pyramid_oereb.tests.conftest import MockRequest
 from pyramid_oereb import ExtractReader
 from pyramid_oereb import MunicipalityReader
@@ -120,57 +120,70 @@ def test_processor_without_images(connection):
 
 
 def test_processor_get_legend_entries():
-    assert True
-    # theme1 = ThemeRecord(u'TEST', {'de': 'Theme 1'})
-    # # theme2 = ThemeRecord(u'TEST', {'de': 'Theme 2'})
-    # office = OfficeRecord({'de': 'Test Office'})
-    # legend1 = LegendEntryRecord(ImageRecord(bin(1)), {'de': 'legend1'}, u'type1', u'bla', theme1)
-    # legend2 = LegendEntryRecord(ImageRecord(bin(1)), {'de': 'legend2'}, u'type2', u'bla', theme1)
-    # # legend3 = LegendEntryRecord(ImageRecord(bin(1)), {'de': 'legend3'}, u'type3', u'bla', theme2)
-    # # legend4 = LegendEntryRecord(ImageRecord(bin(1)), {'de': 'legend4'}, u'type4', u'bla', theme2)
-    # view_service1 = ViewServiceRecord(
-    #     'http://www.test1.url.ch',
-    #     'http://www.test1.url.ch',
-    #     legends=[legend1, legend2]
-    # )
-    # # view_service2 = ViewServiceRecord(
-    # #     'http://www.test2.url.ch',
-    # #     'http://www.test2.url.ch',
-    # #     legends=[legend3, legend4]
-    # # )
-    # image = ImageRecord(bin(1))
-    # plr1 = PlrRecord(
-    #     theme1,
-    #     {'de': 'CONTENT'},
-    #     u'inForce',
-    #     datetime.datetime.now(),
-    #     office,
-    #     image,
-    #     type_code=u'type1',
-    #     view_service=view_service1
-    # )
-    # plr2 = PlrRecord(
-    #     theme1,
-    #     {'de': 'CONTENT'},
-    #     u'inForce',
-    #     datetime.datetime.now(),
-    #     office,
-    #     image,
-    #     type_code=u'type2',
-    #     view_service=view_service1
-    # )
-    # # plr3 = PlrRecord(
-    # #     theme1,
-    # #     {'de': 'CONTENT'},
-    # #     u'inForce',
-    # #     datetime.datetime.now(),
-    # #     office,
-    # #     image,
-    # #     type_code=u'type2',
-    # #     view_service=view_service1
-    # # )
-    #
-    # # inside_plrs = [plr1]
-    # # outside_plrs = [plr2]
-    # # after_process = Processor.get_legend_entries(inside_plrs, outside_plrs)
-    # TODO: add more specific tests here
+    theme1 = ThemeRecord(u'TEST', {'de': 'Theme 1'})
+    theme2 = ThemeRecord(u'TEST', {'de': 'Theme 2'})
+    office = OfficeRecord({'de': 'Test Office'})
+    legend1 = LegendEntryRecord(ImageRecord(bin(1)), {'de': 'legend1'}, u'type1', u'bla', theme1)
+    legend2 = LegendEntryRecord(ImageRecord(bin(1)), {'de': 'legend2'}, u'type2', u'bla', theme1)
+    legend3 = LegendEntryRecord(ImageRecord(bin(1)), {'de': 'legend3'}, u'type3', u'bla', theme2)
+    legend4 = LegendEntryRecord(ImageRecord(bin(1)), {'de': 'legend4'}, u'type4', u'bla', theme2)
+    view_service1 = ViewServiceRecord(
+        'http://www.test1.url.ch',
+        'http://www.test1.url.ch',
+        legends=[legend1, legend2]
+    )
+    view_service2 = ViewServiceRecord(
+        'http://www.test2.url.ch',
+        'http://www.test2.url.ch',
+        legends=[legend3, legend4]
+    )
+    image = ImageRecord(bin(1))
+    plr1 = PlrRecord(
+        theme1,
+        {'de': 'CONTENT'},
+        u'inForce',
+        datetime.datetime.now(),
+        office,
+        image,
+        type_code=u'type1',
+        view_service=view_service1
+    )
+    plr2 = PlrRecord(
+        theme1,
+        {'de': 'CONTENT'},
+        u'inForce',
+        datetime.datetime.now(),
+        office,
+        image,
+        type_code=u'type2',
+        view_service=view_service1
+    )
+    plr3 = PlrRecord(
+        theme1,
+        {'de': 'CONTENT'},
+        u'inForce',
+        datetime.datetime.now(),
+        office,
+        image,
+        type_code=u'type2',
+        view_service=view_service2
+    )
+    plr4 = PlrRecord(
+        theme1,
+        {'de': 'CONTENT'},
+        u'inForce',
+        datetime.datetime.now(),
+        office,
+        image,
+        type_code=u'type2',
+        view_service=view_service2
+    )
+
+    inside_plrs = [plr1]
+    outside_plrs = [plr2, plr3]
+    after_process = Processor.get_legend_entries(inside_plrs, outside_plrs)
+    assert len(inside_plrs) == len(after_process)
+    inside_plrs = [plr3]
+    outside_plrs = [plr4]
+    after_process = Processor.get_legend_entries(inside_plrs, outside_plrs)
+    assert len(after_process) == 1
