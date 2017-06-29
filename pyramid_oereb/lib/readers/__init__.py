ed80042c5925fc42fba3bb24d095e7845749b943
Clemens Rudert <clemens.rudert@bl.ch>
2017-06-29 15:03:20 +0200

    GSOREB-168 #time 15m add module doc

--- pyramid_oereb/lib/readers/__init__.py
+++ pyramid_oereb/lib/readers/__init__.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+
+"""
+Regarding to the main documentation you find here all the stuff which is related to the reader layer. This
+layer is part of the core server application. It is addressed directly by the processor. Its main task is to
+call the user configured sources and read all the necessary records from them. This is the lowest core layer.
+
+.. note:: This layer is not meant to be adapted in normal.
+"""
