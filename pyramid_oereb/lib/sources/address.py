e9aa59d720e69a841283fec9f2a48fcf40e97d2d
Karsten Deininger <karsten.deininger@bl.ch>
2017-07-04 16:12:15 +0200

    Connect GeoAdmin API and add tests
    GSOREB-187 #time 2h 30m Connect GeoAdmin API and add tests.

--- pyramid_oereb/lib/sources/address.py
+++ pyramid_oereb/lib/sources/address.py
@@ -2,7 +2,9 @@
 import requests
 from geoalchemy2.elements import _SpatialElement
 from geoalchemy2.shape import to_shape
+from pyreproj import Reprojector
 
+from pyramid_oereb.lib.config import Config
 from pyramid_oereb.lib.sources import BaseDatabaseSource, Base
 from pyramid_oereb.lib.records.address import AddressRecord
 
@@ -51,26 +53,25 @@ class AddressGeoAdminSource(AddressBaseSource):
     """
     An address source that uses the federal GeoAdmin API to return the geo location of a postal address.
 
-    Args:
-        kwargs (dict): Keyword arguments to configure the source:
-
-            - geoadmin_search_api (uri): Url of the GeoAdmin API's search service. (**required**)
-            - origins (str or list of str): Filter results by origin. Defaults to *address*. (**optional**)
-            - proxies (dict): Proxy definition according to
-                http://docs.python-requests.org/en/master/user/advanced/#proxies. (**optional**)
+    Keyword Args:
+        geoadmin_search_api (uri): Url of the GeoAdmin API's search service. (**required**)
+        origins (str or list of str): Filter results by origin. Defaults to *address*. (**optional**)
+        proxies (dict): Proxy definition according to
+            http://docs.python-requests.org/en/master/user/advanced/#proxies. (**optional**)
     """
     def __init__(self, **kwargs):
         super(AddressGeoAdminSource, self).__init__()
-        self._geoadmin_url_ = kwargs.get('geoadmin_search_api')
-        self._type_ = 'locations'
-        self._proxies_ = kwargs.get('proxies')
+        self._geoadmin_url = kwargs.get('geoadmin_search_api',
+                                        'https://api3.geo.admin.ch/rest/services/api/SearchServer')
+        self._type = 'locations'
+        self._proxies = kwargs.get('proxies')
         if 'origins' in kwargs:
             origins = kwargs.get('origins')
             if isinstance(origins, list):
                 origins = ','.join(origins)
-            self._origins_ = origins
+            self._origins = origins
         else:
-            self._origins_ = 'address'
+            self._origins = 'address'
 
     def read(self, street_name, zip_code, street_number):
         """
@@ -79,21 +80,32 @@ class AddressGeoAdminSource(AddressBaseSource):
         Args:
             street_name (unicode): The name of the street for the desired address.
             zip_code (int): The postal zipcode for the desired address.
-            street_number (str): The house or so called street number of the desired address.
+            street_number (unicode): The house or so called street number of the desired address.
         """
-        params = {
-            'type': self._type_,
-            'origins': self._origins_,
-            'searchText': '{0} {1} {2}'.format(zip_code, street_name, street_number)
+        headers = {
+            'Referer': 'http://bl.ch'  # TODO: Remove this header when referer is not needed anymore!
         }
-        response = requests.get(self._geoadmin_url_, params=params, proxies=self._proxies_)
+        params = {
+            'type': self._type,
+            'origins': self._origins,
+            'searchText': u'{0} {1} {2}'.format(zip_code, street_name, street_number)
+        }
+        response = requests.get(self._geoadmin_url, params=params, proxies=self._proxies, headers=headers)
         if response.status_code == requests.codes.ok:
+            rp = Reprojector()
+            srid = Config.get('srid')
             self.records = list()
             data = response.json()
             if 'results' in data:
-                for address in data.get('results'):
-                    # TODO: Create address record(s) from response when geometry is available.
-                    pass
-                    # self.records.append(AddressRecord())
+                for item in data.get('results'):
+                    attrs = item.get('attrs')
+                    if isinstance(attrs, dict) and attrs.get('origin') == 'address':
+                        x, y = rp.transform((attrs.get('lon'), attrs.get('lat')), to_srs=srid)
+                        self.records.append(AddressRecord(
+                            street_name=street_name,
+                            zip_code=zip_code,
+                            street_number=street_number,
+                            geom='POINT({x} {y})'.format(x=x, y=y)
+                        ))
         else:
             response.raise_for_status()
