028a08e03ce160eab6e184b68d796ca7119f1a23
Stéphane Brunner <stephane.brunner@camptocamp.com>
2017-07-19 10:42:25 +0200

    Manage localised text

--- pyramid_oereb/lib/renderer/extract/print.py
+++ pyramid_oereb/lib/renderer/extract/print.py
@@ -33,13 +33,39 @@ class PrintRenderer(Renderer):
             return HTTPBadRequest("With image is not allowed in the print")
 
         self._request = self.get_request(system)
+
+        self.default_lang = Config.get('default_language')
+        if 'lang' in self._request.GET:
+            self.lang = self._request.GET.get('lang')
+        else:
+            self.lang = self.default_lang
+
         extract_dict = self._render(value[0], value[1])
         self._flatten_object(extract_dict, 'PLRCadastreAuthority')
         self._flatten_object(extract_dict, 'RealEstate')
         self._flatten_object(extract_dict, 'RealEstate_RestrictionOnLandownership')
+        self._flatten_object(extract_dict, 'RealEstate_RestrictionOnLandownership_Theme')
         self._flatten_object(extract_dict, 'RealEstate_RestrictionOnLandownership_ResponsibleOffice')
         self._flatten_array_object(
             extract_dict, 'RealEstate_RestrictionOnLandownership_Geometry', 'ResponsibleOffice')
+        for attr_name in ['NotConcernedTheme', 'ThemeWithoutData', 'ConcernedTheme']:
+            for theme in extract_dict[attr_name]:
+                self._localised_text(theme, 'Text')
+        self._localised_text(extract_dict, 'RealEstate_RestrictionOnLandownership_Theme_Text')
+        self._multilingual_m_text(extract_dict, 'GeneralInformation')
+        self._multilingual_m_text(extract_dict, 'BaseData')
+        for item in extract_dict.get('Glossary', []):
+            self._multilingual_text(item, 'Title')
+            self._multilingual_text(item, 'Content')
+        self._multilingual_m_text(extract_dict, 'RealEstate_RestrictionOnLandownership_Information')
+        self._multilingual_text(extract_dict, 'RealEstate_RestrictionOnLandownership_ResponsibleOffice_Name')
+        for item in extract_dict.get('RealEstate_RestrictionOnLandownership_Geometry', []):
+            self._multilingual_text(item, 'ResponsibleOffice_Name')
+        self._multilingual_text(extract_dict, 'PLRCadastreAuthority_Name')
+        for item in extract_dict.get('ExclusionOfLiability', []):
+            self._multilingual_text(item, 'Title')
+            self._multilingual_text(item, 'Content')
+
         extract_dict["features"] = {
             "features": {
                 "type": "FeatureCollection",
@@ -104,3 +130,17 @@ class PrintRenderer(Renderer):
             for key, value in parent[name].items():
                 parent['{}_{}'.format(name, key)] = value
             del parent[name]
+
+    def _localised_text(self, parent, name):
+        if name in parent:
+            parent[name] = parent[name]['Text']
+
+    def _multilingual_m_text(self, parent, name):
+        self._multilingual_text(parent, name)
+
+    def _multilingual_text(self, parent, name):
+        if name in parent:
+            # lang_obj = dict([(e['Language'], e['Text']) for e in parent[name]])
+            # lang = self.lang if self.lang in lang_obj else self.default_lang
+            # parent[name] = lang_obj[lang]
+            parent[name] = parent[name][0]['Text']
