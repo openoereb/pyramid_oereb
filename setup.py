a29cfc7a982f24c632c288fd2ae82e499f3eb517
Karsten Deininger <karsten.deininger@bl.ch>
2017-07-20 15:46:47 +0200

    Test requirements are defined in requirements.txt

--- setup.py
+++ setup.py
@@ -9,13 +9,6 @@ with open(os.path.join(here, 'README.rst')) as f:
 with open(os.path.join(here, 'CHANGES.rst')) as f:
     CHANGES = f.read()
 
-tests_require = [
-    'WebTest >= 1.3.1',  # py3 compat
-    'pytest',  # includes virtualenv
-    'pytest-cov',
-    'pytest-ordering'
-]
-
 requires = [
     'dicttoxml',
     'geoalchemy2',
@@ -63,15 +56,10 @@ setup(
         'standard/logo_confederation.png',
         'standard/logo_oereb.png',
         'standard/logo_canton.png',
-        'standard/templates/plr.py.mako',
-        'tests/resources/*',
-        'tests/resources/plr119/*'
+        'standard/templates/plr.py.mako'
     ]},
     include_package_data=True,
     zip_safe=False,
-    extras_require={
-        'testing': tests_require,
-    },
     install_requires=requires,
     entry_points={
         'paste.app_factory': [
