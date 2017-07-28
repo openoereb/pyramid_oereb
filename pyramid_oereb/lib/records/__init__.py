cc6c4491ee61565a9de28879108d024ffcd13314
Clemens Rudert <clemens.rudert@posteo.de>
2017-07-28 14:43:07 +0200

    refine doc

--- pyramid_oereb/lib/records/__init__.py
+++ pyramid_oereb/lib/records/__init__.py
@@ -2,7 +2,7 @@
 """
 Regarding to the main documentation you find here all the stuff which is related to the records. You can
 imagine these record things as the in-application-currency. They are achieved and structured in the
-:ref:`sources`. They are gathered by the :ref:`readers`. And they are combined, sorted, dismissed and
+:ref:`base-sources`. They are gathered by the :ref:`readers`. And they are combined, sorted, dismissed and
 reorganized by the processor.
 
 .. note:: This layer is not meant to be adapted in normal.
