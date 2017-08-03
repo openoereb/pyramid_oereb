56e6ac20985e19ea0ae71150662dc342f81406bb
Clemens Rudert <clemens.rudert@bl.ch>
2017-08-03 10:46:27 +0200

    GSOREB-126 #time 10m add python 3.6 as supported language

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
