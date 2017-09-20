71c1978a63975b346950a58432a95cf69ffc03e2
Stéphane Brunner <stephane.brunner@camptocamp.com>
2017-09-20 08:18:49 +0200

    Rework

--- pyramid_oereb/contrib/print_proxy/mapfish_print.py
+++ pyramid_oereb/contrib/print_proxy/mapfish_print.py
@@ -66,15 +66,13 @@ class Renderer(JsonRenderer):
         self._flatten_object(extract_dict, 'RealEstate')
 
         url, params = parse_url(extract_dict['RealEstate_PlanForLandRegister']['ReferenceWMS'])
-        extract_dict['baseLayers'] = {
-            'layers': [{
-                'type': 'wms',
-                'baseURL': urlparse.urlunsplit((url.scheme, url.netloc, url.path, None, None)),
-                'layers': params['LAYERS'][0].split(','),
-                'imageFormat': 'image/png',
-            }]
+        basemap = {
+            'type': 'wms',
+            'baseURL': urlparse.urlunsplit((url.scheme, url.netloc, url.path, None, None)),
+            'layers': params['LAYERS'][0].split(','),
+            'imageFormat': 'image/png',
         }
-        basemap = extract_dict['baseLayers']['layers'][0]
+        extract_dict['baseLayers'] = {'layers': [basemap]}
         extract_dict['legend'] = extract_dict['RealEstate_PlanForLandRegister'].get('LegendAtWeb', '')
         del extract_dict['RealEstate_PlanForLandRegister']  # /definitions/Map
 
