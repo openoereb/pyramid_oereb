9279d2004a72780f7d96f596720fe58d7df3f128
Wolfgang Kaltz <wolfgang.kaltz@camptocamp.com>
2018-07-24 14:22:56 +0200

    because 1.2.0 was already used for an upload on pypi.org (the first beta version), and because pypi.org disallows using a name more then once even after deleting a package (see https://pypi.org/help/#file-name-reuse), we unfortunately need to explicitly set a different version number in our code

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [re_.match(r).group(1) for r in recommend]
 
 setup(
     name='pyramid_oereb',
-    version='1.2.0',
+    version='1.2.0-rc.2',
     description='pyramid_oereb, extension for pyramid web frame work to provide '
             'a basic server part for the oereb project',
     long_description='{readme}\n\n{changes}'.format(readme=README, changes=CHANGES),
