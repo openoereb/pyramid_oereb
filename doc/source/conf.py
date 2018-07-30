04d0c3e0faf1aa0a42a813f5b58fb364a65a7286
Wolfgang Kaltz <wolfgang.kaltz@camptocamp.com>
2018-07-30 16:36:41 +0200

    remove unused and faulty parameter to avoid warning

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -166,7 +166,7 @@ html_show_sourcelink = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['doc/_buildstatic']
+# html_static_path = ['doc/_buildstatic']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
