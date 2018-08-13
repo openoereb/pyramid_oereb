bbea908a50ae579cc29751e87f1bc8f0b830a3db
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2018-08-13 14:03:34 +0200

    Remove useless todo in model
    GSOREB-250

--- pyramid_oereb/standard/models/motorways_building_lines.py
+++ pyramid_oereb/standard/models/motorways_building_lines.py
@@ -94,7 +94,7 @@ class DataIntegration(Base):
     office = relationship(Office)
 
 
-class ReferenceDefinition(Base):  # TODO: Check translation
+class ReferenceDefinition(Base):
     """
     The meta bucket for definitions which are directly related to a public law restriction in a common way or
     to the whole canton or a  whole municipality. It is used to have a place to store general documents
@@ -393,7 +393,7 @@ class Geometry(Base):
     id = sa.Column(sa.String, primary_key=True, autoincrement=False)
     law_status = sa.Column(sa.String, nullable=False)
     published_from = sa.Column(sa.Date, nullable=False)
-    geo_metadata = sa.Column(sa.String, nullable=True)  # TODO: Check translation
+    geo_metadata = sa.Column(sa.String, nullable=True)
     geom = sa.Column(GeoAlchemyGeometry('LINESTRING', srid=srid), nullable=False)
     public_law_restriction_id = sa.Column(
         sa.String,
@@ -533,7 +533,6 @@ class PublicLawRestrictionDocument(Base):
     article_numbers = sa.Column(sa.String, nullable=True)
 
 
-# TODO: check translation
 class DocumentReference(Base):
     """
     Meta bucket (join table) for the relationship between documents.
