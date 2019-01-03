27460b8515422cd9fb3649573bbcb5a7ab50bb78
Wolfgang Kaltz <wolfgang.kaltz@camptocamp.com>
2019-01-03 09:46:45 +0100

    increment version (for next daily builds)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [re_.match(r).group(1) for r in recommend]
 
 setup(
     name='pyramid_oereb',
-    version='1.3.0',
+    version='1.3.1',
     description='pyramid_oereb, extension for pyramid web frame work to provide '
             'a basic server part for the oereb project',
     long_description='{readme}\n\n{changes}'.format(readme=README, changes=CHANGES),
