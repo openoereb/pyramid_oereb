58d1173908c04691b8eb98b3f56f3905ca832ff4
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2019-01-10 16:38:04 +0100

    Set fake Config.srid in doc
    Issue #666 and #681

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,12 +20,15 @@ import glob
 import inspect
 import os
 from mako.template import Template
-# import sys
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import re
 import six
 import subprocess
 import sphinx_rtd_theme
+from pyramid_oereb.lib.config import Config
+
+Config._config = {'srid': -1}
 
 
 # -- General configuration ------------------------------------------------
@@ -37,7 +40,6 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sys
 
 extensions = [
     'sphinx.ext.coverage',
