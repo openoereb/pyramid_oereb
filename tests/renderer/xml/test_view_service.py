120a2f9b21c66e7efe9c5e8b939db19bf2654a2a
Michael Kuenzli <michael.kuenzli@camptocamp.com>
2019-02-14 18:00:02 +0100

    Fix xml rendering, adapt test

--- tests/renderer/xml/test_view_service.py
+++ tests/renderer/xml/test_view_service.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-from tests import params
 from tests.renderer.xml import xml_templates
 from pyramid_oereb.lib.records.view_service import ViewServiceRecord
+from pyramid_oereb.views.webservice import Parameter
+from pyramid_oereb.lib.renderer.extract.xml_ import Renderer
 
 template = xml_templates().get_template('view_service.xml')
 
 
-@pytest.mark.parametrize('parameters', params)
-def test_empty(parameters):
+def test_empty():
     map = ViewServiceRecord(
         reference_wms='',
         layer_index=0,
@@ -25,8 +24,7 @@ def test_empty(parameters):
     assert len(content) == 4
 
 
-@pytest.mark.parametrize('parameters', params)
-def test_reference_wms(parameters):
+def test_reference_wms():
     map = ViewServiceRecord(
         reference_wms='http://example.com',
         layer_index=0,
@@ -39,16 +37,38 @@ def test_reference_wms(parameters):
     assert len(content) == 5
 
 
-@pytest.mark.parametrize('parameters', params)
-def test_legend_at_web(parameters):
+def test_legend_at_web():
+    parameters = Parameter('reduced', 'xml', False, False, 'BL0200002829', '1000', 'CH775979211712', 'de')
+    renderer = Renderer(None)
     map = ViewServiceRecord(
         reference_wms='',
-        legend_at_web='http://example-legend.com',
+        legend_at_web={'de': 'http://example-legend.com'},
         layer_index=0,
         layer_opacity=1.0
     )
     content = template.render(**{
+        'params': parameters,
+        'localized': renderer.get_localized_text,
         'map': map
     }).decode('utf-8').split('\n')
-    assert content[1] == '<data:LegendAtWeb>http%3A%2F%2Fexample-legend.com</data:LegendAtWeb>'
-    assert len(content) == 5
+    assert len(content) == 6
+    assert content[2] == '<data:LegendAtWeb>http%3A%2F%2Fexample-legend.com</data:LegendAtWeb>'
+
+
+def test_legend_at_web_no_language():
+    # Requests italian, but only german is available -> best effort: deliver german instead
+    parameters = Parameter('reduced', 'xml', False, False, 'BL0200002829', '1000', 'CH775979211712', 'it')
+    renderer = Renderer(None)
+    map = ViewServiceRecord(
+        reference_wms='',
+        legend_at_web={'de': 'http://example-legend.com'},
+        layer_index=0,
+        layer_opacity=1.0
+    )
+    content = template.render(**{
+        'params': parameters,
+        'localized': renderer.get_localized_text,
+        'map': map
+    }).decode('utf-8').split('\n')
+    assert len(content) == 6
+    assert content[2] == '<data:LegendAtWeb>http%3A%2F%2Fexample-legend.com</data:LegendAtWeb>'
