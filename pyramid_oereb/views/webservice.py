9cef49e2cbe9b0908e81f45c881c4ef42966ee32
Wolfgang Kaltz <wolfgang.kaltz@camptocamp.com>
2019-09-19 17:37:15 +0200

    format trigger for purely technical reasons (re-trigger Codacy)

--- pyramid_oereb/views/webservice.py
+++ pyramid_oereb/views/webservice.py
@@ -445,8 +445,8 @@ class PlrWebservice(object):
 
 
 class Parameter(object):
-    def __init__(self, flavour, format, with_geometry, images, identdn=None, number=None, egrid=None,
-                 language=None, topics=None):
+    def __init__(self, flavour, format, with_geometry, images, identdn=None, number=None,
+                 egrid=None, language=None, topics=None):
         """
         Creates a new parameter instance.
 
