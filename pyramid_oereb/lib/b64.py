6645df08ec42f101713f8d2d58b432044c1a61d3
Karsten Deininger <karsten.deininger@bl.ch>
2019-10-16 11:56:49 +0200

    Add function docs

--- pyramid_oereb/lib/b64.py
+++ pyramid_oereb/lib/b64.py
@@ -7,6 +7,15 @@ PY3 = sys.version_info[0] >= 3
 
 
 def encode(value):
+    """
+    Encodes the input value using base64 encoding.
+
+    Args:
+        value (basestring): The input value.
+
+    Returns:
+        basestring: The encoded value.
+    """
     if PY3 and isinstance(value, str):
         encoded = base64.b64encode(value.encode('utf-8'))
     else:
@@ -18,6 +27,15 @@ def encode(value):
 
 
 def decode(value):
+    """
+    Decodes the input value using base64 encoding.
+
+    Args:
+        value (basestring): The input value.
+
+    Returns:
+        basestring: The decoded value.
+    """
     if PY3:
         return base64.b64decode(value.encode('ascii'))
     else:
