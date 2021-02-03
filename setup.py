bb63da4b88680a4bed628347904d52ff3591fc2d
requires.io <support@requires.io>
2021-02-03 10:39:14 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
