9b4049ad77a59b2abb8b7820b2146e50e5463d3c
Karsten Deininger <karsten.deininger@bl.ch>
2021-07-01 09:21:19 +0000

    Add index attribute

--- pyramid_oereb/lib/records/documents.py
+++ pyramid_oereb/lib/records/documents.py
@@ -13,11 +13,12 @@ class DocumentRecord(object):
     Attributes:
         document_type (str or unicode): The document type. It must be "LegalProvision", "Law" or "Hint"
                 every other value will raise an error.
+        index (int): An index used to sort the documents.
         law_status (unicode):  Key string of the law status.
-        published_from (datetime.date): Date since this document was published.
         title (dict of unicode): The multilingual title of the document. It might be shortened one.
         responsible_office (pyramid_oereb.lib.records.office.OfficeRecord): Office which is
             responsible for this document.
+        published_from (datetime.date): Date since this document was published.
         text_at_web (dict of uri): The multilingual URI to the documents content.
         abbreviation (dict of unicode): Short term for this document (multilingual).
         official_number (dict of unicode): The official number for identification of this document.
@@ -26,19 +27,20 @@ class DocumentRecord(object):
         file (bytes): The binary content of the document.
         references (list of DocumentRecord): The references to other documents.
     """
-    def __init__(self, document_type, law_status, published_from, title, responsible_office, text_at_web=None,
-                 abbreviation=None, official_number=None, only_in_municipality=None, article_numbers=None,
-                 file=None, references=None):
+    def __init__(self, document_type, index, law_status, title, responsible_office, published_from,
+                 text_at_web=None, abbreviation=None, official_number=None, only_in_municipality=None,
+                 article_numbers=None, file=None, references=None):
         """
 
         Args:
             document_type (str or unicode): The document type. It must be "LegalProvision", "Law" or "Hint"
                 every other value will raise an error.
+            index (int): An index used to sort the documents.
             law_status (pyramid_oereb.lib.records.law_status.LawStatusRecord): The law status of this record.
-            published_from (datetime.date): Date since this document was published.
             title (dict of unicode): The multilingual title of the document. It might be shortened one.
             responsible_office (pyramid_oereb.lib.records.office.OfficeRecord): Office which is
                 responsible for this document.
+            published_from (datetime.date): Date since this document was published.
             text_at_web (dict of uri): The multilingual URI to the documents content.
             abbreviation (dict of unicode): Short term for this document (multilingual).
             official_number (dict of unicode): The official number for identification of this document.
@@ -69,6 +71,7 @@ class DocumentRecord(object):
 
         self.document_type = document_type
         self.title = title
+        self.index = index
         self.responsible_office = responsible_office
         self.abbreviation = abbreviation
         self.official_number = official_number
--- tests/records/test_document.py
+++ tests/records/test_document.py
@@ -15,9 +15,10 @@ def test_mandatory_fields():
 def test_init():
     office_record = OfficeRecord({'en': 'name'})
     law_status = LawStatusRecord.from_config(u'inForce')
-    record = DocumentRecord('GesetzlicheGrundlage', law_status, datetime.date(1985, 8, 29), {'en': 'title'},
-                            office_record, {'en': 'http://my.document.com'})
+    record = DocumentRecord('GesetzlicheGrundlage', 1, law_status, {'en': 'title'}, office_record,
+                            datetime.date(1985, 8, 29), text_at_web={'en': 'http://my.document.com'})
     assert isinstance(record.document_type, str)
+    assert isinstance(record.index, int)
     assert isinstance(record.law_status, LawStatusRecord)
     assert isinstance(record.published_from, datetime.date)
     assert isinstance(record.title, dict)
@@ -34,16 +35,16 @@ def test_init():
 def test_invalid_document_type():
     office_record = OfficeRecord({'en': 'name'})
     with pytest.raises(AttributeError):
-        DocumentRecord('invalid', 'runningModifications',
-                       datetime.date(1985, 8, 29), {'en': 'title'}, office_record)
+        DocumentRecord('invalid', 1, 'runningModifications', {'en': 'title'}, office_record,
+                       datetime.date(1985, 8, 29))
 
 
 def test_future_document():
     office_record = OfficeRecord({'en': 'name'})
     law_status = LawStatusRecord.from_config(u'inForce')
-    record = DocumentRecord('Hinweis', law_status,
-                            (datetime.datetime.now().date() + datetime.timedelta(days=7)), {'en': 'title'},
-                            office_record, {'en': 'http://my.document.com'})
+    record = DocumentRecord('Hinweis', 1, law_status, {'en': 'title'}, office_record,
+                            (datetime.datetime.now().date() + datetime.timedelta(days=7)),
+                            text_at_web={'en': 'http://my.document.com'})
     assert not record.published
 
 
@@ -51,13 +52,14 @@ def test_init_with_relation():
     office_record = OfficeRecord({'en': 'name'})
     law_status = LawStatusRecord.from_config(u'inForce')
     references = [
-        DocumentRecord('GesetzlicheGrundlage', law_status, datetime.date(1985, 8, 29), {'de': 'Titel 1'},
-                       office_record, {'en': 'http://my.document.com'})
+        DocumentRecord('GesetzlicheGrundlage', 2, law_status, {'de': 'Titel 1'}, office_record,
+                       datetime.date(1985, 8, 29), text_at_web={'en': 'http://my.document.com'})
     ]
-    record = DocumentRecord('Hinweis', law_status, datetime.date(1985, 8, 29), {'de': 'title'},
-                            office_record, {'en': 'http://my.document.com'}, references=references,
-                            article_numbers=['test'])
+    record = DocumentRecord('Hinweis', 1, law_status, {'de': 'title'}, office_record,
+                            datetime.date(1985, 8, 29), text_at_web={'en': 'http://my.document.com'},
+                            references=references, article_numbers=['test'])
     assert isinstance(record.document_type, str)
+    assert isinstance(record.index, int)
     assert isinstance(record.law_status, LawStatusRecord)
     assert isinstance(record.published_from, datetime.date)
     assert isinstance(record.title, dict)
@@ -73,10 +75,7 @@ def test_init_with_relation():
 def test_legal_provision():
     office_record = OfficeRecord({'en': 'name'})
     law_status = LawStatusRecord.from_config(u'inForce')
-    legal_provision = DocumentRecord('Rechtsvorschrift',
-                                     law_status,
-                                     datetime.date(1985, 8, 29),
-                                     {'de': 'title'},
-                                     office_record)
+    legal_provision = DocumentRecord('Rechtsvorschrift', 1, law_status, {'de': 'title'},
+                                     office_record, datetime.date(1985, 8, 29))
     assert isinstance(legal_provision.document_type, str)
     assert legal_provision.document_type == 'Rechtsvorschrift'
