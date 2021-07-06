516fe0f15a5baa933c9b660e7b580201bfd19f82
Karsten Deininger <karsten.deininger@bl.ch>
2021-07-06 10:00:26 +0000

    Remove tests for removed classes

--- tests/xtf_import/test_reference_definition.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from pyramid_oereb.standard.xtf_import.reference_definition import ReferenceDefinition
-
-
-def test_init():
-    reference_definition = ReferenceDefinition('foo', 'bar', 'baz')
-    assert reference_definition._session == 'foo'
-    assert reference_definition._model == 'bar'
-    assert reference_definition._topic_code == 'baz'
