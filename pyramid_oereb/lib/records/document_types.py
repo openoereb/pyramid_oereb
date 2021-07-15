19dbb6fb76cbd98f630c2fcb4ed15dcc66deeca7
voisardf <francois.voisard@ne.ch>
2021-07-15 15:01:55 +0200

    new record element for document types

---
+++ pyramid_oereb/lib/records/document_types.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import warnings
+
+
+class DocumentTypeRecord(object):
+    """
+    Represents a document type entry with the code and it's display name.
+
+    Args:
+        code (str of unicode): The code.
+        text (dict of unicode): The label text for the document type (multilingual).
+    """
+    def __init__(self, code, text):
+
+        if not isinstance(code, str):
+            warnings.warn('Type of "code" should be "str"')
+        if not isinstance(text, dict):
+            warnings.warn('Type of "text" should be "dict"')
+
+        self.code = code
+        self.text = text
+        
