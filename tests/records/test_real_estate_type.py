6624e361202307f2deedd1f2f5e8292392edbd42
Anne Maier <anne.maier@bs.ch>
2021-07-26 08:59:37 +0200

    fix linting

--- tests/records/test_real_estate_type.py
+++ tests/records/test_real_estate_type.py
@@ -1,5 +1,6 @@
 from pyramid_oereb.lib.records.real_estate_type import RealEstateTypeRecord
 
+
 def test_real_estate_type_init():
     record = RealEstateTypeRecord(u'code', {u'de': u'Liegenschaft'})
     assert record.code == u'code'
