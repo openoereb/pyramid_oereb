342dec241fa50331018f4b7e111261c2d3a8661d
Karsten Deininger <karsten.deininger@bl.ch>
2021-08-03 07:59:43 +0000

    Remove deprecated routes

--- pyramid_oereb/routes.py
+++ pyramid_oereb/routes.py
@@ -47,64 +47,12 @@ def includeme(config):  # pragma: no cover
         decorator=log_response
     )
 
-    # Get version - Can be removed if backward compatibility no longer required.
-    config.add_route('{0}/versions.json'.format(route_prefix), '/versions.json')
-    config.add_view(
-        PlrWebservice,
-        attr='get_versions',
-        route_name='{0}/versions.json'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-    config.add_route('{0}/versions'.format(route_prefix), '/versions')
-    config.add_view(
-        PlrWebservice,
-        attr='get_versions',
-        route_name='{0}/versions'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-    config.add_route('{0}/versions_old/'.format(route_prefix), '/versions/')
-    config.add_view(
-        PlrWebservice,
-        attr='get_versions',
-        route_name='{0}/versions_old/'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-
     # Get capabilities
     config.add_route('{0}/capabilities/'.format(route_prefix), '/capabilities/{format}')
     config.add_view(
         PlrWebservice,
         attr='get_capabilities',
         route_name='{0}/capabilities/'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-
-    # Get capabilities - Can be removed if backward compatibility no longer required.
-    config.add_route('{0}/capabilities.json'.format(route_prefix), '/capabilities.json')
-    config.add_view(
-        PlrWebservice,
-        attr='get_capabilities',
-        route_name='{0}/capabilities.json'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-    config.add_route('{0}/capabilities'.format(route_prefix), '/capabilities')
-    config.add_view(
-        PlrWebservice,
-        attr='get_capabilities',
-        route_name='{0}/capabilities'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-    config.add_route('{0}/capabilities_old'.format(route_prefix), '/capabilities/')
-    config.add_view(
-        PlrWebservice,
-        attr='get_capabilities',
-        route_name='{0}/capabilities_old'.format(route_prefix),
         request_method='GET',
         decorator=log_response
     )
@@ -134,65 +82,6 @@ def includeme(config):  # pragma: no cover
         PlrWebservice,
         attr='get_egrid_address',
         route_name='{0}/getegrid_address/'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-
-    # Get egrid - Can be removed if backward compatibility no longer required.
-    config.add_route('{0}/getegrid_coord.json'.format(route_prefix),
-                     '/getegrid.json')
-    config.add_route('{0}/getegrid_ident.json'.format(route_prefix),
-                     '/getegrid/{identdn}/{number}.json')
-    config.add_route('{0}/getegrid_address.json'.format(route_prefix),
-                     '/getegrid/{postalcode}/{localisation}/{number}.json')
-    config.add_view(
-        PlrWebservice,
-        attr='get_egrid_coord',
-        route_name='{0}/getegrid_coord.json'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-    config.add_view(
-        PlrWebservice,
-        attr='get_egrid_ident',
-        route_name='{0}/getegrid_ident.json'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-    config.add_view(
-        PlrWebservice,
-        attr='get_egrid_address',
-        route_name='{0}/getegrid_address.json'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-    config.add_route('{0}/getegrid_coord'.format(route_prefix), '/getegrid')
-    config.add_route('{0}/getegrid_ident'.format(route_prefix), '/getegrid/{identdn}/{number}')
-    # This legacy route (old specification) can't work anymore
-    # because of the one with {format} so it's
-    # commented and the view removed.
-    # config.add_route('{0}/getegrid_address'.format(route_prefix),
-    #                 '/getegrid/{postalcode}/{localisation}/{number}')
-    config.add_view(
-        PlrWebservice,
-        attr='get_egrid_coord',
-        route_name='{0}/getegrid_coord'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-    config.add_view(
-        PlrWebservice,
-        attr='get_egrid_ident',
-        route_name='{0}/getegrid_ident'.format(route_prefix),
-        request_method='GET',
-        decorator=log_response
-    )
-
-    config.add_route('{0}/getegrid_coord_old/'.format(route_prefix), '/getegrid/')
-    config.add_view(
-        PlrWebservice,
-        attr='get_egrid_coord',
-        route_name='{0}/getegrid_coord_old/'.format(route_prefix),
         request_method='GET',
         decorator=log_response
     )
