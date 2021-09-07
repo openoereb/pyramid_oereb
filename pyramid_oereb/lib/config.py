2fb6502865affc346723275347134656c905d194
Marion <marion.baumgartner@camptocamp.com>
2021-09-07 15:09:35 +0200

    remove old sub-theme sorter

--- pyramid_oereb/lib/config.py
+++ pyramid_oereb/lib/config.py
@@ -821,31 +821,6 @@ class Config(object):
         )
 
     @staticmethod
-    def get_sub_theme_sorter_config(theme_code):
-        assert Config._config is not None
-        sorter = {
-            'module': 'pyramid_oereb.contrib.print_proxy.sub_themes.sorting',
-            'class_name': 'BaseSort',
-            'params': {}
-        }
-        themes = Config._config.get('plrs')
-        if themes and isinstance(themes, list):
-            for theme in themes:
-                if theme.get('code') == theme_code:
-                    sub_themes = theme.get('sub_themes', {})
-                    if 'sorter' in sub_themes:
-                        sorter = sub_themes.get('sorter')
-                    break
-        # Check if sorter is valid
-        if 'module' not in sorter:
-            log.error("Invalid configuration for sub theme sorter for theme {}, "
-                      "no module property".format(theme_code))
-        if 'class_name' not in sorter:
-            log.error("Invalid configuration for sub theme sorter for theme {}, "
-                      "no class_name property".format(theme_code))
-        return sorter
-
-    @staticmethod
     def extract_module_function(dotted_function_path):
         elements = dotted_function_path.split('.')
         function_name = elements[-1]
