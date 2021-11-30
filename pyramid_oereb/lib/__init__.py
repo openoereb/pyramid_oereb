7c1c2a57a7aef1833baeb65bd1afa455cac0af22
Wolfgang Kaltz <wolfgang.kaltz@camptocamp.com>
2021-11-30 19:48:31 +0100

    Add more logs upon error (#1389)

--- pyramid_oereb/lib/__init__.py
+++ pyramid_oereb/lib/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import traceback
 
 from pyramid.httpexceptions import HTTPInternalServerError
 
@@ -13,6 +14,10 @@ def get_multilingual_element(value, language, not_null=True):
         msg = 'Default language "{language}" is not available in: \n {element}'\
             .format(language=language, element=value)
         log.error(msg)
+        log.info("Call stack of failing get_multilingual_element:")
+        for line in traceback.format_stack():
+            log.info("{}".format(line))
+
         raise HTTPInternalServerError()
 
     return multilingual_value
