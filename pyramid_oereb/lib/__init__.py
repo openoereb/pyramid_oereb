0176f2c7b7287e51e97bf41fe031f39adc66349a
Anne Maier <anne.maier@bs.ch>
2021-11-30 11:41:02 +0100

    add docstrings to core/init.py

--- pyramid_oereb/lib/__init__.py
+++ pyramid_oereb/lib/__init__.py
@@ -7,6 +7,22 @@ log = logging.getLogger(__name__)
 
 
 def get_multilingual_element(value, language, not_null=True):
+    """
+        Method that takes a sting or dict in order to retrieve the text
+        value of the requested language.
+
+        Args:
+            values (str or dict): The multilingual values encoded as JSON.
+            language (str): The language in which the multilingual element
+                is to be returned.
+
+        Returns:
+            String: The equivalent string in the requested language.
+
+        Raises:
+            HTTPInternalServerError: If default language of requested value
+                is not available.
+        """
     multilingual_value = value.get(language, None)
 
     if multilingual_value is None and not_null:
