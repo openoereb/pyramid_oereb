2a9c5792193e6e5383acf56ae940f5966441797f
Michael Mueller <michael.mueller@bl.ch>
2021-12-03 09:56:00 +0100

    add function (remove duplicated records)

--- pyramid_oereb/contrib/__init__.py
+++ pyramid_oereb/contrib/__init__.py
@@ -1,1 +1,51 @@
 # -*- coding: utf-8 -*-
+
+def eliminate_duplicated_document_records(main_document_records, plr_document_records):
+    """ Filtering of document records that are associated to a plr.
+
+    Document records associated to a plr are eliminated if a record associated to a theme exists
+    for the same document. Document records associated to a theme have priority.
+    Records are considered to handle the same document if:
+    - indices are equal, and
+    - document_type codes are equal, and
+    - official_numbers correspond
+
+    Correct data is expected.
+    """
+
+    # basic rules (one or the other source does not provide any document records)
+    if main_document_records is None and len(plr_document_records) > 0:
+        return plr_document_records
+    if main_document_records is not None and len(plr_document_records) == 0:
+        return main_document_records
+
+    # list which indicates duplicated documents
+    plr_document_is_duplicated_list = [False] * len(plr_document_records)
+
+    # document per document comparison
+    for doc1 in main_document_records:
+        for index2, doc2 in enumerate(plr_document_records):
+            if plr_document_is_duplicated_list[index2] is False:
+                # comparison of indices
+                if doc1.index != doc2.index:
+                    continue
+                # comparison of document_type
+                if doc1.document_type.code != doc2.document_type.code:
+                    continue
+                # comparison of number
+                # - Note: official number is NOT mandatory
+                # - possibility of not corresponding languages
+                docs_have_same_number = False
+                if doc1.official_number is not None and doc2.official_number is not None:
+                    for key, value in doc1.official_number.items():
+                        if doc2.official_number.get(key):
+                            if doc2.official_number.get(key) == value:
+                                docs_have_same_number = True
+                                break
+                    if docs_have_same_number:
+                        plr_document_is_duplicated_list[index2] = True
+
+    unique_document_indexes = [i for i, val in enumerate(plr_document_is_duplicated_list) if val is False]
+    unique_document_records = [plr_document_records[i] for i in unique_document_indexes]
+
+    return main_document_records + unique_document_records
