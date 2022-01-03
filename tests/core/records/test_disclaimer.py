712de6630862954d838bfd1160894a80b23048fe
vvmruder <crud3_rt@posteo.ch>
2022-01-03 16:40:42 +0100

    fix typo

--- tests/core/records/test_disclaimer.py
+++ tests/core/records/test_disclaimer.py
@@ -28,7 +28,7 @@ def record_correct():
 
 @pytest.fixture
 def record_incorrect():
-    yield DisclaimerRecord('titel', 'content')
+    yield DisclaimerRecord('title', 'content')
 
 
 def test_init(record_correct):
