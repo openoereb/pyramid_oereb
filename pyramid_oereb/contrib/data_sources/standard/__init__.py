ee77c66ad6791a9d993dce733d32ecb17442c0de
Clemens Rudert <crud3_rt@posteo.ch>
2022-01-28 21:41:24 +0100

    remove unused code

--- pyramid_oereb/contrib/data_sources/standard/__init__.py
+++ pyramid_oereb/contrib/data_sources/standard/__init__.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import re
 from sqlalchemy.schema import CreateTable
 from sqlalchemy.dialects import postgresql
-
-
-def convert_camel_case_to_snake_case(name):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
-
-
-def convert_camel_case_to_text_form(name):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1 \2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1 \2', s1)
 
 
 def tables(base):
