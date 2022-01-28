5bce189ce39329a01e39dc8fbe9fe4834ee3a911
vvmruder <vvmruder@users.noreply.github.com>
2022-01-28 16:38:01 +0100

    Change collection of legend entries (#1482)
    * fix legend handling in plr standard source
    
    * fix lint issues
    
    * add dev data to have example of other legend, fix other legend handling

--- pyramid_oereb/contrib/data_sources/standard/sources/plr.py
+++ pyramid_oereb/contrib/data_sources/standard/sources/plr.py
@@ -128,7 +128,14 @@ class DatabaseSource(BaseDatabaseSource, PlrBaseSource):
             session.close()
 
     def from_db_to_legend_entry_record(self, legend_entry_from_db):
-        theme = Config.get_theme_by_code_sub_code(legend_entry_from_db.theme, legend_entry_from_db.sub_theme)
+        theme = Config.get_theme_by_code_sub_code(legend_entry_from_db.theme)
+        if legend_entry_from_db.sub_theme:
+            sub_theme = Config.get_theme_by_code_sub_code(
+                legend_entry_from_db.theme,
+                legend_entry_from_db.sub_theme
+            )
+        else:
+            sub_theme = None
         legend_entry_record = self._legend_entry_record_class(
             ImageRecord(b64.decode(legend_entry_from_db.symbol)),
             legend_entry_from_db.legend_text,
@@ -136,17 +143,19 @@ class DatabaseSource(BaseDatabaseSource, PlrBaseSource):
             legend_entry_from_db.type_code_list,
             theme,
             view_service_id=legend_entry_from_db.view_service_id,
+            sub_theme=sub_theme
         )
         return legend_entry_record
 
-    def from_db_to_legend_entry_records(self, legend_entries_from_db, sub_theme=None):
+    def from_db_to_legend_entry_records(self, legend_entries_from_db, plr_legend_entry):
         legend_entry_records = []
-        if sub_theme is not None:
-            for legend_entry_from_db in legend_entries_from_db:
-                if legend_entry_from_db.sub_theme == sub_theme:
-                    self.from_db_to_legend_entry_record(legend_entry_from_db)
-        else:
-            for legend_entry_from_db in legend_entries_from_db:
+        theme_code = plr_legend_entry.theme.code
+        sub_theme_code = None
+        if plr_legend_entry.sub_theme:
+            sub_theme_code = plr_legend_entry.sub_theme.sub_code
+
+        for legend_entry_from_db in legend_entries_from_db:
+            if legend_entry_from_db.sub_theme == sub_theme_code and legend_entry_from_db.theme == theme_code:
                 legend_entry_records.append(
                     self.from_db_to_legend_entry_record(legend_entry_from_db)
                 )
@@ -313,20 +322,20 @@ class DatabaseSource(BaseDatabaseSource, PlrBaseSource):
         legend_entry_record = self.from_db_to_legend_entry_record(
             public_law_restriction_from_db.legend_entry
         )
+
+        # this list holds all records which are belonging to the dedicated
+        # PLR (it is decided by the PLR's theme and sub_theme).
         legend_entry_records = self.from_db_to_legend_entry_records(
             legend_entries_from_db,
-            legend_entry_record.theme.sub_code
+            legend_entry_record
         )
         symbol = ImageRecord(b64.decode(public_law_restriction_from_db.legend_entry.symbol))
         view_service_record = self.from_db_to_view_service_record(
             public_law_restriction_from_db.view_service,
             legend_entry_records
         )
-        theme = Config.get_theme_by_code_sub_code(
-            public_law_restriction_from_db.legend_entry.theme,
-            public_law_restriction_from_db.legend_entry.sub_theme)
         document_records = eliminate_duplicated_document_records(
-            theme.document_records,
+            legend_entry_record.theme.document_records,
             self.get_document_records(params, public_law_restriction_from_db)
         )
         geometry_records = self.from_db_to_geometry_records(public_law_restriction_from_db.geometries)
@@ -334,10 +343,8 @@ class DatabaseSource(BaseDatabaseSource, PlrBaseSource):
             self._plr_info.get('code'),
             public_law_restriction_from_db.law_status
         )
-        sub_theme_legend_entry_record = \
-            legend_entry_record.theme if legend_entry_record.theme.sub_code else None
         plr_record = self._plr_record_class(
-            Config.get_theme_by_code_sub_code(legend_entry_record.theme.code),
+            legend_entry_record.theme,
             legend_entry_record,
             law_status,
             public_law_restriction_from_db.published_from,
@@ -346,7 +353,7 @@ class DatabaseSource(BaseDatabaseSource, PlrBaseSource):
             symbol,
             view_service_record,
             geometry_records,
-            sub_theme=sub_theme_legend_entry_record,
+            sub_theme=legend_entry_record.sub_theme,
             type_code=public_law_restriction_from_db.legend_entry.type_code,
             type_code_list=public_law_restriction_from_db.legend_entry.type_code_list,
             documents=document_records,
@@ -465,15 +472,12 @@ class DatabaseSource(BaseDatabaseSource, PlrBaseSource):
         Returns:
             list: The result of the related geometries unique by the public law restriction id
         """
-        distinct_legend_entry_ids = []
-        geometries = self.handle_collection(session, bbox).distinct(
-            self._model_.public_law_restriction_id
-        ).all()
 
+        distinct_legend_entry_ids = []
+        geometries = self.handle_collection(session, bbox).all()
         for geometry in geometries:
             if geometry.public_law_restriction.legend_entry_id not in distinct_legend_entry_ids:
                 distinct_legend_entry_ids.append(geometry.public_law_restriction.legend_entry_id)
-
         return session.query(self.legend_entry_model).filter(
             self.legend_entry_model.id.in_((distinct_legend_entry_ids))).all()
 
