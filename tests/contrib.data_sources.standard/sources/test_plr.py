a1f98efd998b99e0e7fff58c292be1fca6e93211
vvmruder <crud3_rt@posteo.ch>
2022-02-08 22:56:03 +0100

    further improve tests of plr source

--- tests/contrib.data_sources.standard/sources/test_plr.py
+++ tests/contrib.data_sources.standard/sources/test_plr.py
@@ -5,16 +5,17 @@ from sqlalchemy import String
 from sqlalchemy.orm import declarative_base
 
 from shapely.geometry import Polygon, GeometryCollection
-
+from pyramid_oereb.core.config import Config
 from pyramid_oereb.contrib.data_sources.standard.models import get_view_service, get_legend_entry
 from pyramid_oereb.contrib.data_sources.standard.sources.plr import DatabaseSource
 from pyramid_oereb.core import b64
+from pyramid_oereb.core.records.image import ImageRecord
 from pyramid_oereb.core.records.theme import ThemeRecord
-from pyramid_oereb.core.records.view_service import LegendEntryRecord
+from pyramid_oereb.core.records.view_service import LegendEntryRecord, ViewServiceRecord
 
 
 @pytest.fixture
-def source_params(db_connection):
+def plr_source_params(db_connection):
     yield {
         "code": "ch.Nutzungsplanung",
         "geometry_type": "GEOMETRYCOLLECTION",
@@ -44,7 +45,8 @@ def source_params(db_connection):
             "class": "pyramid_oereb.contrib.data_sources.standard.sources.plr.DatabaseSource",
             "params": {
                 "db_connection": db_connection,
-                "model_factory": "pyramid_oereb.contrib.data_sources.standard.models.theme.model_factory_string_pk",  # noqa: E501
+                "model_factory": "pyramid_oereb.contrib.data_sources.standard.models.theme.model_factory_string_pk",
+                # noqa: E501
                 "schema_name": "land_use_plans"
             }
         },
@@ -82,7 +84,7 @@ def source_params(db_connection):
 
 
 @pytest.fixture(autouse=True)
-def config(app_config):
+def config_themes(app_config):
     themes = [
         ThemeRecord(
             "ch.Nutzungsplanung",
@@ -103,9 +105,18 @@ def config(app_config):
         yield
 
 
-@pytest.fixture
-def all_result_session(session, query):
+@pytest.fixture(autouse=True)
+def config_config(app_config):
+    mock_config = {
+        'default_language': 'de',
+        'srid': 2056
+    }
+    with patch('pyramid_oereb.core.config.Config._config', mock_config):
+        yield
 
+
+@pytest.fixture
+def all_plr_result_session(session, query):
     class Query(query):
 
         def all(self):
@@ -124,37 +135,135 @@ def all_result_session(session, query):
 
 
 @pytest.fixture
-def legend_entry_model_class():
-    Base = declarative_base()
-    ViewService = get_view_service(Base, 'test', String)
-    yield get_legend_entry(Base, 'test', String, ViewService)
+def base():
+    yield declarative_base()
 
 
-@pytest.mark.parametrize('legend_entry_params', [{
+@pytest.fixture
+def view_service_model_class(base):
+    yield get_view_service(base, 'test', String)
+
+
+@pytest.fixture
+def legend_entry_model_class(view_service_model_class, base):
+    yield get_legend_entry(base, 'test', String, view_service_model_class)
+
+
+@pytest.fixture
+def legend_entries_from_db(legend_entry_model_class, png_binary):
+    yield [legend_entry_model_class(**{
         'id': '1',
-        'legend_text': {'de': 'testlegende'},
+        'legend_text': {'de': 'testlegende without sub theme'},
         'type_code': 'testCode',
         'type_code_list': 'testCode,testCode2,testCode3',
         'theme': 'ch.Nutzungsplanung',
         'sub_theme': None,
-        'view_service_id': '1'
-    }, {
+        'view_service_id': '1',
+        'symbol': b64.encode(png_binary)
+    }), legend_entry_model_class(**{
         'id': '1',
-        'legend_text': {'de': 'testlegende'},
+        'legend_text': {'de': 'testlegende with sub theme'},
         'type_code': 'testCode',
         'type_code_list': 'testCode,testCode2,testCode3',
         'theme': 'ch.Nutzungsplanung',
         'sub_theme': "ch.Subcode",
-        'view_service_id': '1'
-    }])
-def test_from_db_to_legend_entry_record(source_params, all_result_session, legend_entry_model_class, png_binary, legend_entry_params):  # noqa: E501
-    legend_entry_params.update({'symbol': b64.encode(png_binary)})
-    legend_entry_from_db = legend_entry_model_class(**legend_entry_params)
-    with patch('pyramid_oereb.core.adapter.DatabaseAdapter.get_session', return_value=all_result_session()):
-        source = DatabaseSource(**source_params)
-        legend_entry_record = source.from_db_to_legend_entry_record(legend_entry_from_db)
+        'view_service_id': '1',
+        'symbol': b64.encode(png_binary)
+    })]
+
+
+@pytest.fixture
+def view_service_from_db(view_service_model_class):
+    yield view_service_model_class(**{
+        'id': '1',
+        'reference_wms': {'de': 'https://geowms.bl.ch/?'
+                                'SERVICE=WMS&'
+                                'version=1.1.1&'
+                                'REQUEST=GetMap&'
+                                'layers=grundkarte_farbig_group&'
+                                'bbox=2614821,1259276,2618821,1263276&'
+                                'width=600&'
+                                'height=600&'
+                                'srs=EPSG:2056&'
+                                'format=image/png'},
+        'layer_index': 1,
+        'layer_opacity': 1.0
+    })
+
+
+@pytest.fixture
+def legend_entry_records(png_binary):
+    yield [
+        LegendEntryRecord(
+            ImageRecord(png_binary),
+            {'de': 'plr_legend_entry text'},
+            'testCode3',
+            'testCode,testCode2,testCode3',
+            Config.themes[0],
+            1,
+            Config.themes[1]
+        ),
+        LegendEntryRecord(
+            ImageRecord(png_binary),
+            {'de': 'plr_legend_entry text'},
+            'testCode3',
+            'testCode,testCode2,testCode3',
+            Config.themes[0],
+            1
+        )
+    ]
+
+def test_from_db_to_legend_entry_record_with_subtheme(plr_source_params, all_plr_result_session, legend_entries_from_db, png_binary):  # noqa: E501
+    with patch('pyramid_oereb.core.adapter.DatabaseAdapter.get_session', return_value=all_plr_result_session()):
+        source = DatabaseSource(**plr_source_params)
+        legend_entry_record = source.from_db_to_legend_entry_record(legend_entries_from_db[1])
         assert isinstance(legend_entry_record, LegendEntryRecord)
         assert legend_entry_record.theme.code == 'ch.Nutzungsplanung'
+        assert legend_entry_record.symbol.content == ImageRecord(b64.decode(b64.encode(png_binary))).content
+        assert legend_entry_record.legend_text == {'de': 'testlegende with sub theme'}
+        assert legend_entry_record.type_code == 'testCode'
+        assert legend_entry_record.type_code_list == 'testCode,testCode2,testCode3'
+        assert isinstance(legend_entry_record.theme, ThemeRecord)
+        assert legend_entry_record.view_service_id == '1'
+        assert isinstance(legend_entry_record.sub_theme, ThemeRecord)
+
+
+def test_from_db_to_legend_entry_record_without_subtheme(plr_source_params, all_plr_result_session, legend_entries_from_db, png_binary):  # noqa: E501
+    with patch('pyramid_oereb.core.adapter.DatabaseAdapter.get_session', return_value=all_plr_result_session()):
+        source = DatabaseSource(**plr_source_params)
+        legend_entry_record = source.from_db_to_legend_entry_record(legend_entries_from_db[0])
+        assert isinstance(legend_entry_record, LegendEntryRecord)
+        assert legend_entry_record.theme.code == 'ch.Nutzungsplanung'
+        assert legend_entry_record.symbol.content == ImageRecord(b64.decode(b64.encode(png_binary))).content
+        assert legend_entry_record.legend_text == {'de': 'testlegende without sub theme'}
+        assert legend_entry_record.type_code == 'testCode'
+        assert legend_entry_record.type_code_list == 'testCode,testCode2,testCode3'
+        assert isinstance(legend_entry_record.theme, ThemeRecord)
+        assert legend_entry_record.view_service_id == '1'
+        assert legend_entry_record.sub_theme is None
+
+
+def test_from_db_to_legend_entry_records_with_subtheme(plr_source_params, all_plr_result_session, legend_entries_from_db, legend_entry_records):
+    with patch('pyramid_oereb.core.adapter.DatabaseAdapter.get_session', return_value=all_plr_result_session()):
+        source = DatabaseSource(**plr_source_params)
+        legend_entry_records = source.from_db_to_legend_entry_records(legend_entries_from_db, legend_entry_records[0])
+        assert len(legend_entry_records) == 1
+        assert legend_entry_records[0].legend_text == {'de': 'testlegende with sub theme'}
+
+
+def test_from_db_to_legend_entry_records_without_subtheme(plr_source_params, all_plr_result_session, legend_entries_from_db, legend_entry_records):
+    with patch('pyramid_oereb.core.adapter.DatabaseAdapter.get_session', return_value=all_plr_result_session()):
+        source = DatabaseSource(**plr_source_params)
+        legend_entry_records = source.from_db_to_legend_entry_records(legend_entries_from_db, legend_entry_records[1])
+        assert len(legend_entry_records) == 1
+        assert legend_entry_records[0].legend_text == {'de': 'testlegende without sub theme'}
+
+
+def test_from_db_to_view_service_record(plr_source_params, all_plr_result_session, legend_entry_records, view_service_from_db):
+    with patch('pyramid_oereb.core.adapter.DatabaseAdapter.get_session', return_value=all_plr_result_session()):
+        source = DatabaseSource(**plr_source_params)
+        view_service_record = source.from_db_to_view_service_record(view_service_from_db, legend_entry_records)
+        assert isinstance(view_service_record, ViewServiceRecord)
 
 
 @pytest.mark.parametrize('tolerances', [None, 0.1, {'ALL': 0.1},
