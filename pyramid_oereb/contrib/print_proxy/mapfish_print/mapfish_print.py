7cd78f6e41e57c5a8f8b7fe5c978561a46adf183
Michael Mueller <michael.mueller@bl.ch>
2022-02-09 16:39:01 +0100

    change var name / update doc

--- pyramid_oereb/contrib/print_proxy/mapfish_print/mapfish_print.py
+++ pyramid_oereb/contrib/print_proxy/mapfish_print/mapfish_print.py
@@ -676,7 +676,9 @@ class Renderer(JsonRenderer):
         """
         Provides the sort key for the supplied legal provision element as a tuple consisting of:
          * title
-         * Official number
+         * official number type ('0': if official number is defined and not an empty string;
+                                 '1': if official number is empty string or not defined)
+         * official number
 
         Args:
             elem (dict): one element of the legal provision list
@@ -686,12 +688,12 @@ class Renderer(JsonRenderer):
 
         sort_title = elem['Title'] if 'Title' in elem else ''
         if 'OfficialNumber' in elem and elem['OfficialNumber']:
-            sort_number_id = 0
+            sort_number_type = 0
             sort_number = elem['OfficialNumber']
         else:
-            sort_number_id = 1
+            sort_number_type = 1
             sort_number = ''
-        return sort_title, sort_number_id, sort_number
+        return sort_title, sort_number_type, sort_number
 
     @staticmethod
     def sort_hints_laws(elem):
