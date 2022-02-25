b7c8bc075f4c24a79b32eb5a5ae2bc2f2a30fc35
Marion <marion.baumgartner@camptocamp.com>
2022-02-25 10:36:27 +0100

    fix doc string in sources #1408

--- pyramid_oereb/contrib/data_sources/oereblex/models/theme.py
+++ pyramid_oereb/contrib/data_sources/oereblex/models/theme.py
@@ -54,6 +54,7 @@ def model_factory(schema_name, pk_type, geometry_type, srid, db_connection):
     class PublicLawRestriction(Base):
         """
         The container where you can fill in all your public law restrictions to the topic.
+
         Attributes:
             id (str): The identifier. This is used in the database only and must not be set manually. If
                 you  don't like it - don't care about.
@@ -113,8 +114,28 @@ def model_factory(schema_name, pk_type, geometry_type, srid, db_connection):
 
 
 def model_factory_string_pk(schema_name, geometry_type, srid, db_connection):
+    """
+    Args:
+        schema_name (str): The name of the database schema where this models belong to.
+        geometry_type (str): The geoalchemy geometry type defined as well known string.
+        srid (int): The SRID defining the projection of the geometries stored in standard db schema.
+        db_connection (str): the db connection string
+
+    Returns:
+        Model: the produced set of standard models
+    """
     return model_factory(schema_name, String, geometry_type, srid, db_connection)
 
 
 def model_factory_integer_pk(schema_name, geometry_type, srid, db_connection):
+    """
+    Args:
+        schema_name (str): The name of the database schema where this models belong to.
+        geometry_type (str): The geoalchemy geometry type defined as well known string.
+        srid (int): The SRID defining the projection of the geometries stored in standard db schema.
+        db_connection (str): the db connection string
+
+    Returns:
+        Model: the produced set of standard models
+    """
     return model_factory(schema_name, Integer, geometry_type, srid, db_connection)
