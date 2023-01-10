88fd22111e65a08c55379371a35852457c477ad0
Wolfgang Kaltz <wolfgang.kaltz@camptocamp.com>
2023-01-10 13:48:48 +0100

    Remove python 3.7 from officially supported python versions (#1670)

--- setup.py
+++ setup.py
@@ -28,9 +28,10 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
